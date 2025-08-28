2c5971236d1650895932ec0fe8592742b3e1a5d5
Saroj Kumar <skumar.k798@gmail.com>
2025-08-28 16:14:36 +0530

    Update DatabaseConnection.java

--- health-care-system/src/main/java/com/healthcare/db/DatabaseConnection.java
+++ health-care-system/src/main/java/com/healthcare/db/DatabaseConnection.java
@@ -7,12 +7,13 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("oracle.jdbc.driver.OracleDriver");
             return DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/HealthcareDB", "root", "dilsad123"
+                    "jdbc:oracle:thin:@localhost:1521:xe", "admin", "manage"
             );
         } catch (Exception e) {
             throw new SQLException("Database connection failed.", e);
         }
     }
+
 }
